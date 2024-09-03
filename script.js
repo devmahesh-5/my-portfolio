78265d86863f88c38d4e539a3c0025881791212a
Mahesh Bhandari <n31mahesh@gmail.com>
2024-09-04 00:19:48 +0545

    frontend almost finished

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const header=document.querySelector(".header");
 const mob_nav=document.querySelector(".mobile");
+const main=document.querySelector(".up");
+// const skillRange=document.querySelector("#skills");
+// const skillLevel=document.querySelector(".skill-level");
 mob_nav.addEventListener('click',()=>{
     header.classList.toggle("active");
+    main.classList.toggle("down");
 })
+// skillRange.addEventListener('load',()=>{
+//     skillLevel.classList.add("skl");
+// })
