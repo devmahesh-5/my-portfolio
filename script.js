eab8aff8038b70f9692c80692fe60a632720844e
Mahesh Bhandari <n31mahesh@gmail.com>
2024-09-05 19:48:37 +0545

    almost done making frontend

--- script.js
+++ script.js
@@ -4,28 +4,24 @@ const main=document.querySelector(".up");
 const chaticon=document.querySelector(".chaticon");
 const available=document.querySelector(".available");
 const answer=document.querySelector(".answer");
-// const skillRange=document.querySelector("#skills");
-// const skillLevel=document.querySelector(".skill-level");
+const intro=document.querySelector(".welcome");
+const cvsection=document.querySelector(".cvsection");
+let nameindex=0;
+let cvindex=0;
+const name="I AM MAHESH BHANDARI.";
+const cv="DOWNLOAD MY CV.";
 mob_nav.addEventListener('click',()=>{
     header.classList.toggle("active");
     main.classList.toggle("down");
 })
-chaticon.addEventListener('mouseover',()=>{
-    chaticon.classList.add("chats");
-})
-chaticon.addEventListener('mouseout',()=>{
-    chaticon.classList.remove("chats");
-})
 
-const messageBot={
-    availableTime:"I am available any Time.Contact Me through Contact page.Thank You.",
-    collaborations:"I am interested in collaboration with tech companies now which offer internship on webdev field.",
-    challenges:"yes I have faced Problems in this field.To Solve,I try AI tools,Documentations and sometime Youtube",
-    timeWorked:"I have been in this field since 2021.",
-    skill:"I have little skill in C and Cpp but I fully dedicate my time in WebdevlopeMent"
-};
-available.addEventListener('click', (event) => {
-    const msg = messageBot["availableTime"];
-    answer.innerHTML += `${msg}`;
-    available.classList.remove();
-});
+let interval=setInterval(() => {
+    if (nameindex < name.length) {
+        intro.innerHTML+=`${name[nameindex++]}`;
+    }else  if(cvindex<cv.length){
+        cvsection.innerHTML+=`${cv[cvindex++]}`;
+    }else{
+        clearInterval(interval)
+    }
+   
+}, 30);
